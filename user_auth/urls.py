994b5272a8d5e443e355e4c6178367b5a1294811
Arpit Rohila <159635013+ArpitRohila045@users.noreply.github.com>
2025-08-22 20:28:38 +0530

    Delete user_auth directory

--- user_auth/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('signup/', views.signup_view, name='signup'),
-    # path('logout/', views.logout_view, name='logout'),
-]
